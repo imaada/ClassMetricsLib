381364b7f207de768a76cb3bd558a3c5a55a275e
Imaad <32370082+imaada@users.noreply.github.com>
2019-12-31 22:13:27 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Meterics
+//
+//  Created by Taha Mian on 2019-12-29.
+//  Copyright © 2019 Taha Mian. All rights reserved.
+//
+
+#include <iostream>
+#include "template.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    // ClassificationType b = BINARY;
+    
+    // ClassificationFatory f;
+    // f.createClassification(b);
+    std::cout << "hellow world" << std::endl;
+    
+    return 0;
+}
+
+
